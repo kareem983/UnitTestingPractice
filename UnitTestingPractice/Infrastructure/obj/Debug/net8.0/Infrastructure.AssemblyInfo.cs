9a73c98fcbd58a5f00ac7dc0d803dab6d7ba2cc0
Kareem Saeed <52586356+kareem983@users.noreply.github.com>
2024-03-25 23:29:53 +0200

    setup hello action

--- UnitTestingPractice/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ UnitTestingPractice/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0977e69c964c290769b31897df49014c6370171f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
